92e006e49f9a31fe69c0c9f2016d4448ca17763a
Theara <ctheara@gmail.com>
2025-07-13 14:28:42 -0400

    Update url to use deployed service

--- frontend/src/features/auth/api/auth-client.js
+++ frontend/src/features/auth/api/auth-client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:5000";
+const baseURL = process.env.REACT_APP_API_BASE_URL ?? "http://localhost:5000";
 
 /**
  * @param userSignUpData object with signup form data
